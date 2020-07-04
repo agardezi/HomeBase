41c9b2ae87b7c7a0d26b7deb1b5bdc62759f4ca5
Ali <alig4lfe@ufl.edu>
2020-07-04 18:34:36 -0400

    initial commit

---
+++ src/test/java/com/projects/homebase/HomebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.homebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
